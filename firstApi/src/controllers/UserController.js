3504802baa0682eca3fb62932ac62be1ca8b2285
brunopapait <brunohpapait@gmail.com>
2020-12-28 22:03:44 -0300

    Recebendo parametros na URL ..

--- firstApi/src/controllers/UserController.js
+++ firstApi/src/controllers/UserController.js
@@ -12,5 +12,18 @@ module.exports = {
     });
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(userOrder));
+  },
+
+  getUserById(req, res) {
+    const { id } = req.params;
+    const user = users.find((item) => item.id === Number(id));
+
+    if (!user) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ error: 'User not found' }));
+    } else {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(user));
+    }
   }
 }
--- firstApi/src/index.js
+++ firstApi/src/index.js
@@ -2,22 +2,32 @@ const http = require('http');
 const routes = require('./routes');
 const url = require('url');
 
-
 const server = http.createServer(function (request, response) {
   const parsedUrl = url.parse(request.url, true);
 
-  request.query = parsedUrl.query;
-  console.log(`Método: ${request.method} | URL: ${parsedUrl.pathname}`);
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split('/').filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+
+
 
   const route = routes.find((routeObj) => (
-    routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
+    routeObj.endpoint === pathname && routeObj.method === request.method
   ));
 
   if (route) {
+    request.query = parsedUrl.query;
+    request.params = { id };
     route.handler(request, response);
   } else {
     response.writeHead(404, { 'Content-Type': 'application/json' });
-    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
+    response.end(`Cannot ${request.method} ${pathname}`);
   }
 });
 
